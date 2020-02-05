164323a383d182be09e56f9c471d249f2f4a7cc3
Mark John Moreno <mark.john.moreno@gmail.com>
2020-02-05 22:52:33 +0800

    implement request id for log tracing and error

---
+++ src/main/java/com/phakk/transit/staticgtfs/api/rest/logging/RequestTraceHeaders.java
@@ -0,0 +1,18 @@
+package com.phakk.transit.staticgtfs.api.rest.logging;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Data
+@Getter
+@Builder
+public class RequestTraceHeaders implements Serializable {
+
+    public static final String HEADER_REQUEST_ID = "x-request-id";
+    public static final String HEADER_REQUEST_PATH = "x-request-path";
+
+    private String requestId;
+}
