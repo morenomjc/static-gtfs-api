f8d1e3df7ca6e276b6a862ab7ab7c6e70b8b67df
Mark John Moreno <mark.john.moreno@gmail.com>
2020-02-15 13:32:57 +0800

    add timestamp in Error and exception handler for NotRouteFound exception

--- src/main/java/com/phakk/transit/staticgtfs/api/rest/advice/ExceptionControllerAdvice.java
+++ src/main/java/com/phakk/transit/staticgtfs/api/rest/advice/ExceptionControllerAdvice.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.time.LocalDateTime;
 
 import static com.phakk.transit.staticgtfs.api.rest.logging.RequestTraceHeaders.HEADER_REQUEST_ID;
 
@@ -36,6 +39,14 @@ public class ExceptionControllerAdvice {
         return mapToApiErrorDto(exception, HttpStatus.INTERNAL_SERVER_ERROR.value(), ErrorCode.ERROR_500_INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ApiDocument handleNoHandlerFoundException(final NoHandlerFoundException exception){
+        logError(exception);
+        return mapToApiErrorDto(exception, HttpStatus.NOT_FOUND.value(), ErrorCode.ERROR_400_NOT_FOUND);
+    }
+
     private void logError(Exception e){
         log.error("Exception encountered caused by: {}", e.getMessage());
     }
@@ -43,6 +54,7 @@ public class ExceptionControllerAdvice {
     private Error mapToErrorDto(Exception exception, int httpStatusCode, ErrorCode errorCode){
         return new Error(
                 MDC.get(HEADER_REQUEST_ID),
+                LocalDateTime.now(),
                 httpStatusCode,
                 errorCode.getCode(),
                 errorCode.getDetail(),
